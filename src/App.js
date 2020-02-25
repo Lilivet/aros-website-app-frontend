14b13c31d1ad971f0d9b769a8978cc049b3eb6cf
Ivett Backlund <ivett.backlund@gmail.com>
2020-02-25 21:59:00 +0100

    all the plugins are installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { quiz } from 'reducers/quiz'
+
+import { CurrentQuestion } from 'components/CurrentQuestion'
+import { Summary } from 'components/Summary'
+
+const reducer = combineReducers({
+
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+
+    </Provider>
   )
 }
